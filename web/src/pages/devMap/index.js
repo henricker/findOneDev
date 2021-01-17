2288411846e83f8ff4a4ca427786eee92ae849a5
henricker <henriquevieira.siqxd@gmail.com>
2021-01-16 21:11:47 -0300

    map added

--- web/src/pages/devMap/index.js
+++ web/src/pages/devMap/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
+import { MapContainer, Marker, TileLayer, Popup  } from 'react-leaflet';
+
 //import components
 import Sidebar from '../../components/sidebar';
-import MapDev from '../../components/map';
+import 'leaflet/dist/leaflet.css';
+import './style.css';
+
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+    const position = [-5.121283598809426, -39.73063941829233];
     return (
         <>
         <div className="map-page">
             <Sidebar className="map-page-aside"/>
-            <MapDev className="map-element"/>
+            <MapContainer center={position} zoom={16}>
+                <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                
+            </MapContainer>
         </div>
         </>
     );
